66f486f6b8481f202352fffd750029dd06c09a92
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-03-27 10:33:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+//DATA TYPES / PRIMITIVES
+
+var string = "string hello there"
+var num = 10 + 20
+var bool = true
+
+var undef = undefined
+
+// DATA STRUCTURES
+//index/indices
+//an index starts at 0
+//          0       1      2   3   4 ..... length is 5
+//ARRAY
+var arr = ["hello", true, 10, [], {}]
+
+//OBJECT
+var obj = {name: "shabnam", cool: true, hobbies : ["hiking", "fishing"]}
+
+//EXPRESSIONS
+
+var expression = 4 >10
+
+console.log(expression)
+
+//IF STATEMENTS
+//if(CONDITION){LOGIC}
+if(expression === true){
+    console.log("ITS TRUE!!!!!!!")
+} else {
+    console.log("IT WAS NOT TRUE!!!! :(")
+}
+
+//+ add
+//- subtract
+//* multiply
+// / divide
+// % remainder (modulus)
